8167e3bf3180b94394b5046694fd11bdc88b71ab
alex_p <86060883+ParkSeongMin1@users.noreply.github.com>
2024-03-25 20:27:07 +0900

    Title: 포탑 부수기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240325/포탑 부수기/destroy-the-turret.py
+++ 240325/포탑 부수기/destroy-the-turret.py
@@ -42,46 +42,40 @@ def set_attacked(attacker):
         return (temp[0][-1], temp[0][-2])
     return []
 
-def find_route(attacked, route, cnt, visited):
+def find_route(attacked, attacker, visited):
     global lazer_route
     global board
     # print(attacked)
-    pos= route[-1]
-    x1, y1 = pos
     # print(x1, y1)
     x2, y2 = attacked
-    for i in range(4):
-        nx, ny = x1+dx[i], y1+dy[i]
-        if nx < 0:
-            nx = n - 1
-        if nx >= n:
-            nx = 0
-        if ny < 0:
-            ny = m - 1
-        if ny >= m:
-            ny = 0
-        # print(nx, ny)
-        if board[nx][ny] == 0 or visited[nx][ny]:
-            continue
-        if (nx, ny) == attacked:
-            print(attacked, (nx, ny))
-            route.append(attacked)
-            print(route)
-            temp = []
-            for i in range(len(route) - 1):
-                temp.append((route[i][0], route[i][1]))
-            if not lazer_route:
-                lazer_route = [temp, cnt+1]
-            else:
-                if lazer_route[-1] > cnt + 1:
-                    lazer_route = [temp, cnt+1]
-            return
-        route.append((nx, ny))
-        visited[nx][ny] = True
-        find_route(attacked, route, cnt+1, visited)
-        route.pop()
-        visited[nx][ny] = False
-
+    queue = deque([[attacker]])
+    min_len = 1000000
+    while queue:
+        routes = queue.popleft()
+        x,y = routes[-1]
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if nx < 0:
+                nx = n - 1
+            if nx >= n:
+                nx = 0
+            if ny < 0:
+                ny = m - 1
+            if ny >= m:
+                ny = 0
+            if board[nx][ny] == 0 or visited[nx][ny]:
+                continue
+            visited[nx][ny] = True
+            n_routes = []
+            for r in routes:
+                n_routes.append(r)
+            n_routes.append((nx, ny))
+            if (nx, ny) == attacked and len(n_routes) < min_len:
+                lazer_route = n_routes
+                min_len = len(n_routes)
+                continue
+            queue.append(n_routes)
+            
 
 def lazer(attacker, attacked):
     global board
@@ -90,12 +84,14 @@ def lazer(attacker, attacked):
     # print(attacked)
     visited = [[False for _ in range(m)] for _ in range(n)]
     visited[x1][y1] = True
-    find_route(attacked, [attacker], 0, visited)
+    find_route(attacked, attacker, visited)
     if not lazer_route:
         bomb(attacker, attacked)
     else:
-        for route in lazer_route[0]:
+        for route in lazer_route:
             x,y = route
+            if (x,y) == attacker:
+                continue
             if (x,y) == (x2, y2):
                 board[x][y] = max(board[x][y] - board[x1][y1], 0)
             else:
@@ -127,4 +123,22 @@ for time in range(1, k+1):
     attacked = set_attacked(attacker)
     # print(attacker, attacked)
     lazer(attacker, attacked)
-    print(lazer_route)
+    if lazer_route:
+        for x in range(n):
+            for y in range(m):
+                if (x,y) not in lazer_route and board[x][y] > 0:
+                    board[x][y] += 1
+    else:
+        for x in range(n):
+            for y in range(m):
+                if (x,y) != attacker and (x,y) != attacked and (x,y) not in bombed and board[x][y] > 0:
+                    board[x][y] += 1
+    lazer_route = []
+    bombed = []
+
+res = 0
+
+for x in range(n):
+    for y in range(m):
+        res = max(res, board[x][y])
+print(res)
